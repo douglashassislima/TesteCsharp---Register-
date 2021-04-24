2c11f1be897ce75c053bd65dc3a04ce35f84625f
douglashassislima <douglashenriquedeassis@gmail.com>
2021-04-24 19:55:48 -0300

    iniciando projeto - teste

---
+++ Teste/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static string Registro()
+        {
+            Console.WriteLine("Registro - Login ");
+            Console.WriteLine("1 - Registro ");
+            Console.WriteLine("2 - Login ");
+            Console.WriteLine("3 - Sair ");
+            string inserir = Console.ReadLine();
+            return inserir; 
+        }
+    }
+}
